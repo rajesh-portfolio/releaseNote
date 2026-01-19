1c361568fc224059722dba63e7ef66cdb1ba9da3
rajesh-portfolio <112622170+rajesh-portfolio@users.noreply.github.com>
2026-01-19 11:15:53 +0530

    update version V7

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@
 
 const build = require('@microsoft/sp-build-web');
 
-// 🔴 Disable ESLint (CI-safe)
-build.addSuppression(/eslint/);
+// ✅ HARD disable lint task (this is the key)
+build.rig.getTasks().get('lint').enabled = false;
 
 build.initialize(require('gulp'));
